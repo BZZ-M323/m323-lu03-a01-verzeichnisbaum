6fda048cd8993fdf2a77231663fdafd4911546a0
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 11:25:07 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+directory_tree = {
+    'type': 'directory',
+    'name': 'root',
+    'path': '/',
+    'children': [
+        {
+            'type': 'directory',
+            'name': 'home',
+            'path': '/home',
+            'children': [
+                {
+                    'type': 'directory',
+                    'name': 'user',
+                    'path': '/home/user',
+                    'children': [
+                        {'type': 'file', 'name': 'file1.txt', 'path': '/home/user/file1.txt'},
+                        {'type': 'file', 'name': 'file2.txt', 'path': '/home/user/file2.txt'},
+                    ],
+                },
+                {'type': 'file', 'name': 'readme.md', 'path': '/home/readme.md'},
+            ],
+        },
+        {
+            'type': 'directory',
+            'name': 'etc',
+            'path': '/etc',
+            'children': [
+                {'type': 'file', 'name': 'config.yaml', 'path': '/etc/config.yaml'},
+                {
+                    'type': 'directory',
+                    'name': 'nginx',
+                    'path': '/etc/nginx',
+                    'children': [
+                        {'type': 'file', 'name': 'nginx.conf', 'path': '/etc/nginx/nginx.conf'},
+                        {
+                            'type': 'directory',
+                            'name': 'sites-enabled',
+                            'path': '/etc/nginx/sites-enabled',
+                            'children': [
+                                {'type': 'file', 'name': 'default', 'path': '/etc/nginx/sites-enabled/default'}
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
 
+
+def find_file(name, directory):
+    # Your code goes here
+    pass
 
 if __name__ == '__main__':
-    calculate()
+    path = find_file('config.yaml', directory_tree)
+    print(path)  # Sollte den Pfad zur Datei ausgeben
--- main_test.py
+++ main_test.py
@@ -1,6 +1,73 @@
 import main
+def test_find_file_found_at_root():
+    """Testet, ob die Datei im Wurzelverzeichnis gefunden wird."""
+    directory_tree = {
+        'type': 'directory',
+        'name': 'root',
+        'path': '/',
+        'children': [
+            {'type': 'file', 'name': 'root_file.txt', 'path': '/root_file.txt'},
+        ],
+    }
+    path = main.find_file('root_file.txt', directory_tree)
+    assert path == '/root_file.txt'
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_find_file_found_in_subdir():
+    """Testet, ob die Datei in einem Unterordner gefunden wird."""
+    directory_tree = {
+        'type': 'directory',
+        'name': 'root',
+        'path': '/',
+        'children': [
+            {
+                'type': 'directory',
+                'name': 'subdir',
+                'path': '/subdir',
+                'children': [
+                    {'type': 'file', 'name': 'file_in_subdir.txt', 'path': '/subdir/file_in_subdir.txt'}
+                ]
+            }
+        ]
+    }
+    path = main.find_file('file_in_subdir.txt', directory_tree)
+    assert path == '/subdir/file_in_subdir.txt'
+
+def test_find_file_not_found():
+    """Testet den Fall, dass die Datei nicht gefunden wird."""
+    directory_tree = {
+        'type': 'directory',
+        'name': 'root',
+        'path': '/',
+        'children': [
+            {'type': 'file', 'name': 'another_file.txt', 'path': '/another_file.txt'},
+        ],
+    }
+    path = main.find_file('non_existent.txt', directory_tree)
+    assert path is None
+
+def test_find_file_nested_subdir():
+    """Testet, ob die Datei in einem tief verschachtelten Unterordner gefunden wird."""
+    directory_tree = {
+        'type': 'directory',
+        'name': 'root',
+        'path': '/',
+        'children': [
+            {
+                'type': 'directory',
+                'name': 'subdir',
+                'path': '/subdir',
+                'children': [
+                    {
+                        'type': 'directory',
+                        'name': 'nested_subdir',
+                        'path': '/subdir/nested_subdir',
+                        'children': [
+                            {'type': 'file', 'name': 'nested_file.txt', 'path': '/subdir/nested_subdir/nested_file.txt'}
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+    path = main.find_file('nested_file.txt', directory_tree)
+    assert path == '/subdir/nested_subdir/nested_file.txt'
